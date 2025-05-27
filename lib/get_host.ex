444bf22300cf62e59b5daaeae1b4004197907cd7
Susumu Yamazaki <zacky1972@gmail.com>
2025-05-28 02:19:56 +0900

    chore: remove example hello world code

--- lib/get_host.ex
+++ lib/get_host.ex
@@ -2,17 +2,4 @@ defmodule GetHost do
   @moduledoc """
   Documentation for `GetHost`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GetHost.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/get_host_test.exs
+++ test/get_host_test.exs
@@ -1,8 +1,4 @@
 defmodule GetHostTest do
   use ExUnit.Case
   doctest GetHost
-
-  test "greets the world" do
-    assert GetHost.hello() == :world
-  end
 end
