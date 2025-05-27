f5d82c8acbae8b07c569b2a9a82622788a97a1d4
Susumu Yamazaki <zacky1972@gmail.com>
2025-05-28 02:15:32 +0900

    initial commit

---
+++ lib/get_host.ex
@@ -0,0 +1,18 @@
+defmodule GetHost do
+  @moduledoc """
+  Documentation for `GetHost`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GetHost.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/get_host_test.exs
@@ -0,0 +1,8 @@
+defmodule GetHostTest do
+  use ExUnit.Case
+  doctest GetHost
+
+  test "greets the world" do
+    assert GetHost.hello() == :world
+  end
+end
