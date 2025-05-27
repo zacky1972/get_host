889546ccfb67ac8c1d0b04ceb8ff42f2278c1940
Susumu Yamazaki <zacky1972@gmail.com>
2025-05-28 02:36:08 +0900

    feat: implement hostname_executable/0
    - Add @doc with function description and return value documentation
    - Add function implementation to find hostname executable path

--- lib/get_host.ex
+++ lib/get_host.ex
@@ -2,4 +2,20 @@ defmodule GetHost do
   @moduledoc """
   Documentation for `GetHost`.
   """
+
+  @doc """
+  Finds the path to the `hostname` executable in the system.
+
+  ## Returns
+
+    * `{:ok, path}` - Returns the full `path` to the hostname executable if found
+    * `{:error, reason}` - Returns an error tuple with a `reason` if the executable is not found
+  """
+  @spec hostname_executable() :: {:ok, binary()} | {:error, binary()}
+  def hostname_executable do
+    case System.find_executable("hostname") do
+      nil -> {:error, "Not found \"hostname\""}
+      hostname_cmd -> {:ok, hostname_cmd}
+    end
+  end
 end
