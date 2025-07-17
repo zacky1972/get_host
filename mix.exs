a0b6a389381bf98ae5b3bebe37ee94461abd8d20
Susumu Yamazaki <zacky1972@gmail.com>
2025-07-17 16:07:54 +0900

    chore: Clean up mix.exs and update documentation
    - Remove commented out dependency examples
    - Add CHANGELOG.md to documentation extras

--- mix.exs
+++ mix.exs
@@ -57,8 +57,6 @@ defmodule GetHost.MixProject do
     [
       {:nstandard, "~> 0.1"},
       {:igniter, "~> 0.6", only: [:dev, :test]},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.31", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
@@ -69,7 +67,7 @@ defmodule GetHost.MixProject do
   defp docs do
     [
       main: "GetHost",
-      extras: ["README.md", "LICENSE.md"]
+      extras: ["README.md", "LICENSE.md", "CHANGELOG.md"]
     ]
   end
 
